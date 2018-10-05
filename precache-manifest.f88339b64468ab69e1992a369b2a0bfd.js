bbb99b1501d801868d7ea314a96b113fd7cce6a9
Nicolas Cueva <nicolascueva@outlook.com>
2018-10-05 09:48:32 -0500

    Updates

---
+++ precache-manifest.f88339b64468ab69e1992a369b2a0bfd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "0c405310959817294ecd",
+    "url": "/Clicky-Game/static/js/main.0c405310.chunk.js"
+  },
+  {
+    "revision": "ace62ad78903889e8e0e",
+    "url": "/Clicky-Game/static/js/1.ace62ad7.chunk.js"
+  },
+  {
+    "revision": "0c405310959817294ecd",
+    "url": "/Clicky-Game/static/css/main.8865ba0d.chunk.css"
+  },
+  {
+    "revision": "71d953e86f07636c612226ebbb544bbd",
+    "url": "/Clicky-Game/index.html"
+  }
+];
